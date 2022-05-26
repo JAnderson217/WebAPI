986ff5e7685b8d2b6ae4799ac966d47f3dfec226
JAnderson217 <24385176+JAnderson217@users.noreply.github.com>
2022-05-26 15:34:19 +0100

    Initial commit

---
+++ CRUDWebAPI/Controllers/StudentsController.cs
@@ -0,0 +1,18 @@
+﻿using CRUDLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+namespace CRUDWebAPI.Controllers
+{
+    public class StudentsController : ApiController
+    {
+        CollegeEntities OE = new CollegeEntities();
+        public IQueryable<Student> Get()
+        {
+            return OE.Students;
+        }
+    }
+}
